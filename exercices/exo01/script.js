a558c19078e6484481cacd7e34628fafeebcdc29
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-17 14:55:09 +0100

    Énoncé de l'exercice 01

---
+++ exercices/exo01/script.js
@@ -0,0 +1,10 @@
+/*
+  Exercice 01 
+
+  Créer un programme qui affiche 3 champs: nom, prenom, date de naissance
+  et un bouton.
+  Quand vous cliquez sur le bouton, récupérer les valeurs des champs et les afficher.
+
+  Afficher dans une balise "div" en-dessous les informations sous le format : 
+  - "Bonjour [prenom] [nom], vous êtes né•e le [date]."
+*/
