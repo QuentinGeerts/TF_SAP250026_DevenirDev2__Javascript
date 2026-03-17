7b9b18ae5cc47ed594a50dd243f7f217391f09e5
Quentin Geerts <geerts.quentin@gmail.com>
2026-03-17 16:53:24 +0100

    Correction de l'exercice 01

--- exercices/exo01/script.js
+++ exercices/exo01/script.js
@@ -8,3 +8,26 @@
   Afficher dans une balise "div" en-dessous les informations sous le format : 
   - "Bonjour [prenom] [nom], vous êtes né•e le [date]."
 */
+
+const lastname = document.getElementById("lastname");
+const firstname = document.getElementById("firstname");
+const birthdate = document.getElementById("birthdate");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", handleForm);
+
+function handleForm () {
+
+  const nom = lastname.value.trim();
+  const prenom = firstname.value.trim();
+  const date = birthdate.value;
+
+  if (nom == "" || prenom == "" || date == "") {
+    console.warn("Vous devez remplir les champs.");
+    return;
+  }
+
+  result.textContent = `Bonjour ${prenom} ${nom}, vous êtes né•e le ${date}.`;
+}
+
